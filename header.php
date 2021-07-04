e6ef88f38502badcab7ea18eb9bb5890b8c17a8b
tom <minieyeshsien00@gmail.com>
2021-07-04 22:20:13 +0800

    0704_2

--- header.php
+++ header.php
@@ -22,107 +22,37 @@
 
 <body <?php body_class(); ?>>
 <?php wp_body_open(); ?>
+
 <div id="page" class="site">
 	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'pawsgang' ); ?></a>
 
-	<!-- <div class="announcement-bar">
-		<div class="container">
-		<div class="row">
-			<div class="col-md-4 ">
-				<ul class="announcement-bar__list">
-					<li>
-						<i class="bi bi-telephone rounded-circle"></i>
-						<a href="tel: +44 555 22211"> +44 555 22211</a>
-					</li>
-					<li>
-						<i class="bi bi-envelope rounded-circle"></i>
-						<a href="mailto:hello@pawsgang.shop"> hello@pawsgang.shop</a>
-					</li>
-				</ul>
-			</div>
-			<div class="col-md-8  d-flex justify-content-end">
-				<ul class="announcement-bar__list">
-					<li>
-						<i class="bi bi-truck rounded-circle"></i>FREE SHIPPING
-					</li>
-					<li>
-						<i class="bi bi-clock-history rounded-circle"></i>30DAYS MONEYBACK GURANTEE
-					</li>
-					<li>
-						<i class="bi bi-person rounded-circle"></i>24/7 CUSTOMER SUPPORT
-					</li>
-				</ul>
-			</div>
-		</div>
-		</div>
-	</div> -->
+	<!-- <nav class="navbar navbar-expand-lg navbar-light bg-light mb-0 fixed-top">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Navbar</a>
 
+            <form class="d-flex">
+                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-success" type="submit">Search</button>
+            </form>
 
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
 
-	<header id="masthead" class="site-header">
-		<div class="container pt-2 pb-2">
-			<div class="row align-items-center">
-				<div class="col site-header__logo d-flex justify-content-center ">
-					<!-- <?php the_custom_logo() ?> -->
-					<!-- <img  src="<?php echo get_template_directory_uri();?>/img/logo_black.svg" class="img-fluid" style="width:100px;" loading="lazy" alt="..."> -->
-				</div>	
-			</div>
-		</div>
-
-		<nav class="navbar navbar-expand-lg navbar-light bg-light mb-0 fixed-top">	
-			<div class="container-fluid">
-				<a class="navbar-brand" href="#">Navbar</a>
-
-				<!-- <div class="col-6 col-md-12 cart d-flex justify-content-center  align-items-center m-0 p-0 ">
-						<a href="<?php echo wc_get_cart_url(); ?>" class="cart-icon m-0 p-0"> <i class="bi bi-bag-dash "> </i> </a>
-						<a class="cart-customlocation m-0 p-0" href="<?php echo wc_get_cart_url(); ?> " title="<?php _e( 'View your shopping cart' ); ?>"><?php echo sprintf ( _n( '%d item', '%d items', WC()->cart->get_cart_contents_count() ), WC()->cart->get_cart_contents_count() ); ?> – <?php echo WC()->cart->get_cart_total(); ?></a>
-				</div> -->
-
-				<form class="d-flex">
-					<!-- <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-					<button class="btn btn-outline-success" type="submit">Search</button> -->
-					<a href="<?php echo wc_get_cart_url(); ?>" class="cart-icon m-0 p-0"> <i class="bi bi-bag-dash "> </i> </a>
-						<a class="cart-customlocation m-0 p-0" href="<?php echo wc_get_cart_url(); ?> " title="<?php _e( 'View your shopping cart' ); ?>"><?php echo sprintf ( _n( '%d item', '%d items', WC()->cart->get_cart_contents_count() ), WC()->cart->get_cart_contents_count() ); ?> – <?php echo WC()->cart->get_cart_total(); ?></a>
-				</form>
-
-
-
-				<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-				<span class="navbar-toggler-icon"></span>
-				</button>
-
-				<div class="collapse navbar-collapse " id="navbarSupportedContent">
-						<?php
-						wp_nav_menu(
-							array(
-								'theme_location' => 'menu-1',
-								'menu_id'        => 'primary-menu',
-								)
-							);
-						?>
-				</div>
-			</div>
-		</nav>
-
-		<!-- <nav id="site-navigation" class="main-navigation page fixed-top bg-primary-opacity-8">
-			<div class="container d-flex justify-content-center ">
-				<div class="row justify-content-between mx-0 px-0">
-					
-					<div class="col-6 col-md-12 d-flex justify-content-center align-items-center m-0 p-0 ">
-						<button class="menu-toggle text-center" aria-controls="primary-menu" aria-expanded="false">
-							<i class="bi bi-list "style="font-size: 2rem;"></i>
-							<?php esc_html_e( '', 'pawsgang' ); ?>
-						</button>
-					</div>
-					
-					<div class="col-6 col-md-12 cart d-flex justify-content-center  align-items-center m-0 p-0 ">
-						<a href="<?php echo wc_get_cart_url(); ?>" class="cart-icon m-0 p-0"> <i class="bi bi-bag-dash "> </i> </a>
-						<a class="cart-customlocation m-0 p-0" href="<?php echo wc_get_cart_url(); ?> " title="<?php _e( 'View your shopping cart' ); ?>"><?php echo sprintf ( _n( '%d item', '%d items', WC()->cart->get_cart_contents_count() ), WC()->cart->get_cart_contents_count() ); ?> – <?php echo WC()->cart->get_cart_total(); ?></a>
-					</div>
-
-					
-					<div class="col-12 text-center text-md-center ml-0 d-flex justify-content-center ">
-						<?php
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mx-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Link</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Dropdown
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+							<?php
 						wp_nav_menu(
 							array(
 								'theme_location' => 'menu-1',
@@ -130,11 +60,48 @@
 								)
 							);
 							?>
-					</div>
-					
+                      
+                        </ul>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+		
+    </nav> -->
+
+
+
+
+	<nav id="site-navigation" class="main-navigation page fixed-top bg-primary-opacity-8 ">
+		<div class="container d-flex justify-content-center ">
+			<div class="row justify-content-between mx-0 px-0 w-100">
+				
+				<div class="col-6 col-md-12 d-flex justify-content-s align-items-center m-0 p-0 ">
+					<button class="menu-toggle text-center" aria-controls="primary-menu" aria-expanded="false">
+						<i class="bi bi-list "style="font-size: 2rem;"></i>
+						<?php esc_html_e( '', 'pawsgang' ); ?>
+					</button>
+				</div>
+				<div class="col-6  col-md-3  cart d-flex justify-content-center  align-items-center m-0 p-0 ">
+					<a href="<?php echo wc_get_cart_url(); ?>" class="cart-icon m-0 p-0"> <i class="bi bi-bag-dash "> </i> </a>
+					<a class="cart-customlocation m-0 p-0" href="<?php echo wc_get_cart_url(); ?> " title="<?php _e( 'View your shopping cart' ); ?>"><?php echo sprintf ( _n( '%d item', '%d items', WC()->cart->get_cart_contents_count() ), WC()->cart->get_cart_contents_count() ); ?> – <?php echo WC()->cart->get_cart_total(); ?></a>
+				</div>
+				<div class="col text-center text-md-center m-0 p-0 d-flex justify-content-center justify-content-lg-end">
+					<?php
+					wp_nav_menu(
+						array(
+							'theme_location' => 'menu-1',
+							'menu_id'        => 'primary-menu',
+							)
+						);
+						?>
 				</div>
 			</div>
-		</nav> -->
+		</div>
+	</nav>
 		
 		
 		<!-- #site-navigation -->
